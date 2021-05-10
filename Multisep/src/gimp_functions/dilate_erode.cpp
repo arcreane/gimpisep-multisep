1c4d77006342d3b14f6aa5d4dd9c011ff7146aa0
Algonight <benjamin.fournet99@gmail.com>
2021-05-10 16:42:48 +0200

    [UPDATE] add the user choice

--- Multisep/src/gimp_functions/dilate_erode.cpp
+++ Multisep/src/gimp_functions/dilate_erode.cpp
@@ -31,15 +31,41 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    namedWindow("Dilatation images", WINDOW_AUTOSIZE);
-    namedWindow("Erosion images", WINDOW_AUTOSIZE);
-     
-    createTrackbar("Kernel 2n+1", "Dilatation images", &size_dilatation, 10, Dilatation );
-    createTrackbar("Kernel 2n+1", "Erosion images", &size_erosion, 10, Erosion );
-    Dilatation(0, 0);
-    Erosion(0, 0);
+    cout << "Welcome in the dilate/erode mode" << endl;
+	cout << "Which mode do you want to use ?" << endl;
+	cout << "1 - Dilatation" << endl;
+	cout << "2 - Erosion" << endl;
+    int userChoice;
+	cin >> userChoice;
+	while (cin.fail()) {
+		cout << "This is not an int..." << endl;
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> userChoice;
+	}
 
-    waitKey(0);
-    return 0;
+    switch (userChoice) {
+	case 1 :
+		cout << "Welcome in the Dilatation mode" << endl;
+        cout << "You can change the kernel size with the trackbar at the top of the window" << endl;
+        namedWindow("Dilatation images", WINDOW_AUTOSIZE);
+        createTrackbar("Kernel 2n+1", "Dilatation images", &size_dilatation, 10, Dilatation );
+        Dilatation(0, 0);
+        waitKey(0);
+		break;
+	case 2:
+		cout << "Welcome in the Erosion mode" << endl;
+        cout << "You can change the kernel size with the trackbar at the top of the window" << endl;
+        namedWindow("Erosion images", WINDOW_AUTOSIZE);
+        createTrackbar("Kernel 2n+1", "Erosion images", &size_erosion, 10, Erosion );
+        Erosion(0, 0);
+        waitKey(0);
+		break;
+	default:
+		cout << "This is not a valid option" << endl;
+		break;
+	}
+
     
+    return 0; 
 }

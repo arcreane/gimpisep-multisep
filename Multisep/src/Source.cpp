c772282fe54292ccf19050054afe78522e6eef53
JorisBaudchon <55876397+JorisBaudchon@users.noreply.github.com>
2021-05-10 15:43:39 +0200

    [UPDATE] update du main 1.0; ajout de la classe CheckInt 1.0

--- Multisep/src/Source.cpp
+++ Multisep/src/Source.cpp
@@ -1,21 +1,52 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <sstream>
 
 using namespace cv;
 using namespace std;
 
 int main()
 {
-    Mat image;
-    image = imread("../images/pokemon.jpg");
-    if(image.empty()){
-        cout << "No data" << endl;
-        return -1;
-    }
-    namedWindow("Goddamn Hippies", WINDOW_AUTOSIZE );
-    imshow("Goddamn Hippies", image );
+	cout << "Welcome in your favorite image editor" << endl;
+	cout << "Choose what you want to do with your image" << endl;
+	cout << "Enter the int associated with your choice" << endl;
+	cout << "1 - Dilatation / Erosion" << endl;
+	cout << "2 - Resizing" << endl;
+	cout << "3 - Lighten / Darken" << endl;
+	cout << "4 - Panorama / Stitching" << endl;
+	cout << "5 - Canny Edge Detection" << endl;
 
-    waitKey(0);
+	int userChoice;
+	cin >> userChoice;
+	while (cin.fail()) {
+		cout << "This is not an int..." << endl;
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> userChoice;
+	}
 
+
+	switch (userChoice) {
+	case 1 :
+		cout << "1 - Dilatation / Erosion" << endl;
+		break;
+	case 2:
+		cout << "2 - Resizing" << endl;
+		break;
+	case 3:
+		cout << "3 - Lighten / Darken" << endl;
+		break;
+	case 4:
+		cout << "4 - Panorama / Stitching" << endl;
+		break;
+	case 5:
+		cout << "5 - Canny Edge Detection" << endl;
+		break;
+	default:
+		cout << "This is not a valid option" << endl;
+		break;
+	}
+	String waitingTime;
+	cin >> waitingTime;
     return 0;
 }

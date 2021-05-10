30aef653adb64e7ab911ff3bf45ee2b449686375
Algonight <benjamin.fournet99@gmail.com>
2021-05-10 17:12:34 +0200

    Update CheckInt.cpp

--- Multisep/src/services/CheckInt.cpp
+++ Multisep/src/services/CheckInt.cpp
@@ -1,15 +1,11 @@
 #include <sstream>
 #include <iostream>
-<<<<<<< HEAD
-using namespace std;
-=======
 #include "CheckInt.h"
 
 using namespace std;
 
 int checkInt() {
 	int numberToCheck;
->>>>>>> 8aef9fe4c19c4356cd07bc0ccc26efafcd5242f8
 
 	cin >> numberToCheck;
 	while (cin.fail()) {
